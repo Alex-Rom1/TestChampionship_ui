642cf7b754418fd411d1b63109f5ed0413669537
Alex-rom1 <alex.rom495@gmail.com>
2025-12-06 15:50:52 +0300

    added button big class

---
+++ lib/buttons/button_big.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ButtonBig extends StatelessWidget {
+  final String text;
+  final TextStyle text_style;
+  final Color color_bg;
+  final VoidCallback onPressed;
+
+  ButtonBig({
+    required this.text,
+    required this.text_style,
+    required this.color_bg,
+    required this.onPressed,
+  });
+
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56,
+      width: double.infinity,
+      child: TextButton(
+        onPressed: onPressed,
+        child: Text(text, style: text_style),
+        style: TextButton.styleFrom(
+          backgroundColor: color_bg,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(10),
+          ),
+        ),
+      ),
+    );
+  }
+}
